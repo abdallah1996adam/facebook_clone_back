217f6585fa19c2d9b5f7fec87b90ba9ca54f9ad7
abdallah1996adam <abdallahaadam@protonmail.com>
2021-11-01 21:59:18 +0100

    testing login route :rocket:

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -4,13 +4,34 @@ const bcrypt = require("bcrypt");
 
 const User = require("../models/userModel");
 
-exports.register = async (request, response) => {
-    console.log(request.body);
-    
+exports.register = async (req, res) => {
+  const { username, email, password } = req.body;
+
   try {
-   
-    return response.status(200).json({ message: "OK" });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    
+    const user = await newUser.save();
+    return res.status(200).json({ message: user });
   } catch (error) {
     console.log(error);
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email:email });
+    !user &&  res.status(404).json({ message: "user not found !!!" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: error.message });
   }
 };
--- src/api/routes/userRoutes.js
+++ src/api/routes/userRoutes.js
@@ -1,13 +1,10 @@
-const express = require('express')
+const express = require("express");
 
-const userController = require('../controllers/userController')
-
+const userController = require("../controllers/userController");
 
 const userRouter = express.Router();
 
-
-userRouter.post('/register', userController.register)
-
-
+userRouter.post("/register", userController.register);
+userRouter.post("/login", userController.login);
 
 module.exports = userRouter;
